b610d69708615dba3f8d46e3e8382e0dd456430a
Andrew Reusch <areusch@gmail.com>
2011-11-27 18:28:57 -0800

    Add go mode.

--- dotdir/emacs.d/startup-eval/autoloads.el
+++ dotdir/emacs.d/startup-eval/autoloads.el
@@ -36,8 +36,37 @@ Print number of words in the region.
 
 ;;;***
 
+;;;### (autoloads (gofmt-before-save gofmt go-mode) "go-mode" "../../../../.emacs.d/lazy-loaded/go-mode.el"
+;;;;;;  (20178 61765))
+;;; Generated autoloads from ../../../../.emacs.d/lazy-loaded/go-mode.el
+
+(autoload 'go-mode "go-mode" "\
+Major mode for editing Go source text.
+
+This provides basic syntax highlighting for keywords, built-ins,
+functions, and some types.  It also provides indentation that is
+\(almost) identical to gofmt.
+
+\(fn)" t nil)
+
+(add-to-list 'auto-mode-alist (cons "\\.go$" #'go-mode))
+
+(autoload 'gofmt "go-mode" "\
+Pipe the current buffer through the external tool `gofmt`.
+Replace the current buffer on success; display errors on failure.
+
+\(fn)" t nil)
+
+(autoload 'gofmt-before-save "go-mode" "\
+Add this to .emacs to run gofmt on the current buffer when saving:
+ (add-hook 'before-save-hook #'gofmt-before-save)
+
+\(fn)" t nil)
+
+;;;***
+
 ;;;### (autoloads (google-maps) "google-maps" "../../../../.emacs.d/lazy-loaded/third-party/google-maps/google-maps.el"
-;;;;;;  (20132 62302))
+;;;;;;  (20144 61861))
 ;;; Generated autoloads from ../../../../.emacs.d/lazy-loaded/third-party/google-maps/google-maps.el
 
 (autoload 'google-maps "google-maps" "\
@@ -51,7 +80,7 @@ and do not ask the user for a more precise location.
 
 ;;;### (autoloads (google-maps-geocode-replace-region) "google-maps-geocode"
 ;;;;;;  "../../../../.emacs.d/lazy-loaded/third-party/google-maps/google-maps-geocode.el"
-;;;;;;  (20132 62302))
+;;;;;;  (20144 61861))
 ;;; Generated autoloads from ../../../../.emacs.d/lazy-loaded/third-party/google-maps/google-maps-geocode.el
 
 (autoload 'google-maps-geocode-replace-region "google-maps-geocode" "\
@@ -63,7 +92,7 @@ Geocode region and replace it with a more accurate result.
 
 ;;;### (autoloads (google-maps-static-mode) "google-maps-static"
 ;;;;;;  "../../../../.emacs.d/lazy-loaded/third-party/google-maps/google-maps-static.el"
-;;;;;;  (20132 62302))
+;;;;;;  (20144 61861))
 ;;; Generated autoloads from ../../../../.emacs.d/lazy-loaded/third-party/google-maps/google-maps-static.el
 
 (autoload 'google-maps-static-mode "google-maps-static" "\
@@ -98,7 +127,7 @@ Not documented
 
 ;;;### (autoloads (print-project-definition get-project-definition
 ;;;;;;  project-root-path) "projects" "../../../../.emacs.d/lazy-loaded/projects.el"
-;;;;;;  (20140 36640))
+;;;;;;  (20142 56163))
 ;;; Generated autoloads from ../../../../.emacs.d/lazy-loaded/projects.el
 
 (autoload 'project-root-path "projects" "\
@@ -127,6 +156,23 @@ Locate a project definition file, if any, and read it; if none exists, prompts
 Renames both current buffer and file it's visiting to NEW-NAME.
 
 \(fn NEW-NAME)" t nil)
+
+;;;***
+
+;;;### (autoloads (traverse-accessible-dirs string-matches-regex-list)
+;;;;;;  "traverse-accessible-dirs" "../../../../.emacs.d/lazy-loaded/traverse-accessible-dirs.el"
+;;;;;;  (20142 22271))
+;;; Generated autoloads from ../../../../.emacs.d/lazy-loaded/traverse-accessible-dirs.el
+
+(autoload 'string-matches-regex-list "traverse-accessible-dirs" "\
+Returns t if str matches one of the regexes in list.
+
+\(fn STR LST)" nil nil)
+
+(autoload 'traverse-accessible-dirs "traverse-accessible-dirs" "\
+Traverse accessible, non-blacklisted directories under root. If nil,
+
+\(fn ROOT CALLBACK &optional BLACKLIST PREORDER MAX-DEPTH)" nil nil)
 
 ;;;***
 
@@ -166,6 +212,23 @@ Delete all elements from alist whose car is `equal` to key. Returns the
 Replace the just-yanked text with the text yanked just before it.
 
 \(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("../../../../.emacs.d/lazy-loaded/c-gen.el"
+;;;;;;  "../../../../.emacs.d/lazy-loaded/c-switch-decl-impl.el"
+;;;;;;  "../../../../.emacs.d/lazy-loaded/count-words-region.el"
+;;;;;;  "../../../../.emacs.d/lazy-loaded/go-mode.el" "../../../../.emacs.d/lazy-loaded/js2.el"
+;;;;;;  "../../../../.emacs.d/lazy-loaded/json.el" "../../../../.emacs.d/lazy-loaded/kill-buffers-in-dir.el"
+;;;;;;  "../../../../.emacs.d/lazy-loaded/magit.el" "../../../../.emacs.d/lazy-loaded/projects.el"
+;;;;;;  "../../../../.emacs.d/lazy-loaded/third-party/google-maps/google-maps-base.el"
+;;;;;;  "../../../../.emacs.d/lazy-loaded/third-party/google-maps/google-maps-geocode.el"
+;;;;;;  "../../../../.emacs.d/lazy-loaded/third-party/google-maps/google-maps-static.el"
+;;;;;;  "../../../../.emacs.d/lazy-loaded/third-party/google-maps/google-maps.el"
+;;;;;;  "../../../../.emacs.d/lazy-loaded/third-party/google-maps/org-location-google-maps.el"
+;;;;;;  "../../../../.emacs.d/lazy-loaded/tramp-projects.el" "../../../../.emacs.d/lazy-loaded/traverse-accessible-dirs.el"
+;;;;;;  "../../../../.emacs.d/lazy-loaded/util.el" "../../../../.emacs.d/lazy-loaded/yank-push.el")
+;;;;;;  (20178 61783 594220))
 
 ;;;***
 
