4f5e0099d91babe4bb2edc95f0c8a8fc0d796be2
Andrew Reusch <areusch@gmail.com>
2020-01-06 09:04:19 -0800

    add org-mode startup-evals

---
+++ dotdir/emacs.d/startup-eval/org-agenda.el
@@ -0,0 +1,27 @@
+(require 'org)
+
+;; recursively find .org files in provided directory
+;; modified from an Emacs Lisp Intro example
+(defun sa-find-org-file-recursively (&optional directory filext)
+  "Return .org and .org_archive files recursively from DIRECTORY.
+If FILEXT is provided, return files with extension FILEXT instead."
+  (interactive "DDirectory: ")
+  (let* (org-file-list
+         (case-fold-search t)         ; filesystems are case sensitive
+         (file-name-regex "^[^.#].*") ; exclude dot, autosave, and backupfiles
+         (filext (or filext "org$\\\|org_archive"))
+         (fileregex (format "%s\\.\\(%s$\\)" file-name-regex filext))
+         (cur-dir-list (directory-files directory t file-name-regex)))
+    ;; loop over directory listing
+    (dolist (file-or-dir cur-dir-list org-file-list) ; returns org-file-list
+      (cond
+       ((file-regular-p file-or-dir)             ; regular files
+        (if (string-match fileregex file-or-dir) ; org files
+            (add-to-list 'org-file-list file-or-dir)))
+       ((file-directory-p file-or-dir)
+        (dolist (org-file (sa-find-org-file-recursively file-or-dir filext)
+                          org-file-list) ; add files found to result
+          (add-to-list 'org-file-list org-file)))))))
+
+(add-to-list 'org-agenda-files "~/ws/")
+(setq org-agenda-files (append org-agenda-files (sa-find-org-file-recursively "~/ws/adafruit")))
